f2af9befdee3bab573d5b3eea7cdd1c472eaf96f
Klaus Raphael Machado <67723423+KlausMachado@users.noreply.github.com>
2025-08-20 14:39:30 +0000

    adds initial server setup with Express and CORS - setting Nodemon

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+    res.json({ok : true, timestamp: Date().toString()});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`API is running on  http://localhost:${PORT}`);
+});
